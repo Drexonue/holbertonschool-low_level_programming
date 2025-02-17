49e99f5b353f06ab507aaeb584c5f687d3a1d4a5
root <julerouxyt@outlook.fr>
2025-02-17 15:23:10 +0000

    started the task to printe size of various type on the computer, save it on git to finish it tomorrow

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Entry main
+ * Return: always 0
+*/
+
+int main (void)
+{
+	printf(""Size of char: %d\n", sizeof(char) byte(s)");
+	printf(""Size of int:  %d\n", sizeof(int) byte(s)");
+	printf(""Size of a long int: %d\n", sizeof(long int) byte(s)");
+	printf(""Size of a long long int: %d\n", sizeof(long long int) byte(s)");
+	printf(""Size of a float: %d\n", sizeof(float) byte(s)");
+	return (0);
+}
