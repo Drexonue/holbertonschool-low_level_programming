286e744272439f25e87734fda092bbd020788b5e
root <julerouxyt@outlook.fr>
2025-02-17 13:01:25 +0000

    starting to create a program to wirtea sentence with puts, put it to save it

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+/** main - main block 
+ * Description : prints exactly ""Programming is like building a multilingual puzzle "
+ * Must be followed by a new line 
+ * using fonction  puts
+ * not allow to use printf
+ * program should end with the value 0
+ */
+
+
+
+puts '"Programming is like building a multilingual puzzle'
+
