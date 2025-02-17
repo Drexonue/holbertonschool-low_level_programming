03e1f8030156347807e0fb57b07a8bcef7988460
root <julerouxyt@outlook.fr>
2025-02-17 14:38:36 +0000

    script to write a string with printf

---
+++ hello_world/5-printft.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-write program that prints a string
+ * Return:always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+
