bc4a59d9c2fa69b0c82a510c2666b8474a838bc3
Drexonue <julerouxyt@outlook.fr>
2025-02-19 16:18:26 +0100

    add script to print single digit of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry main
+ *
+ * Return: Always 0
+*/
+
+
+int main(void)
+
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9' ; digit++)
+	{
+		putchar (digit);
+	}
+	putchar ('\n');
+	return (0);
+}
