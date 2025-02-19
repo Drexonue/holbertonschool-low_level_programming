6951277e989a0c53d625cfa62c2e33a3057c165c
Drexonue <julerouxyt@outlook.fr>
2025-02-19 10:53:22 +0100

    Add  the full code to print if a variable is positive or negative, made every modif for betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  *  main -  program to know if a num is pos or neg
- *  Return : 
+ *  Return : Always 0
 */
 
-int main(void) 
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+
+		n = rand() - RAND_MAX / 2;
+		if (n > 0)
+		{
+		printf("%d is postive\n", n); }
+		else
+		{
+		if (n < 0)
+		{
+		printf("%d is negative\n", n); }
+		else
+		{
+		printf("%d is zero\n", n);
+		}}
+		return (0);
+}
+
