d9c81051f5aa0e6060f5999b186d4e2d2aac0ee5
Drexonue <julerouxyt@outlook.fr>
2025-02-20 16:04:23 +0100

    program to print base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry main
+ *
+ * Return : Always 0
+*/
+
+int main(void)
+{
+
+	int num;
+
+	char letter;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar ('\n');
+	return (0);
+}
+
