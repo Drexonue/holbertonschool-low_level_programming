8bb807e9f705eff6230182da1489714501ebe513
root <julerouxyt@outlook.fr>
2025-02-22 14:30:55 +0000

    progra         to print decimals number with a coma between them

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry main
+ *
+ * Return : 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < 10; num++)
+
+	{
+		putchar(num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+		putchar('\n');
+		return (0);
+}
