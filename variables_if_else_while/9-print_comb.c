91661d4e8df6543c556b236bb4e33e91c5366423
root <julerouxyt@outlook.fr>
2025-02-22 15:45:28 +0000

    supressed quote with the first 0 and now it works properly

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,22 +5,21 @@
  * Return : Always 0
 */
 
+
 int main(void)
 {
-	int num;
-
-	for (num = '0'; num < 10; num++)
-
+	int x;
+	
+	for (x = 0; x < 10; x++)
+    {
+	    putchar(x + '0');
+	    if (x < 9)
 	{
-		putchar(num + '0');
-		if (num < 9)
-		{
-			putchar(',');
-			putchar(' ');
-
-		}
+		putchar(',');
+		putchar(' ');
 	}
-
-		putchar('\n');
-		return (0);
+    }
+    putchar('\n');
+    return (0);
 }
+
