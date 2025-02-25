c72e6926cd2b44c3a4391f5194524c21494dfc7e
root <julerouxyt@outlook.fr>
2025-02-25 14:14:23 +0000

    add main.g,putchar and progra to write _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry main
  *
@@ -9,14 +9,15 @@
 int main(void)
 
 {
-	printf("_");
-	printf("p");
-	printf("u");
-	printf("t");
-	printf("c");
-	printf("h");
-	printf("a");
-	printf("r\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
