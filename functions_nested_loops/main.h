4fede0d30cead432b8be6c790e9dafaceb370121
root <julerouxyt@outlook.fr>
2025-02-25 13:15:12 +0000

    add main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  /** Function for _putchar */
+void print_alphabet(void);  /** Declaratin of function print_alphabet */
+void print_alphabet_x10(void); /** Declaration of function print_alphabet_x10 */
+int _islower(int c); /** Function for _islower */
+int _isalpha(int c); /** Function for _iaslpha */
+int print_sign(int n); /** Function for print_sign */
+int _abs(int); /** Function of _abs */
+int print_last_digit(int); /** Function of print_last_digit */
+void jack_bauer(void); /** function that prints every minute of the day */
+void times_table(void); /** function that prints the 9 times tables starting with 0 */
+
+#endif /* MAIN_H */
